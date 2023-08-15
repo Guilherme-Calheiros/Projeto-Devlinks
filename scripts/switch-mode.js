a895fb0f12b8185653d8d70870a9b22736f3ff68
Guilherme-Calheiros <guilhermecalheiros522@gmail.com>
2023-08-15 18:59:11 -0300

    first commit

---
+++ scripts/switch-mode.js
@@ -0,0 +1,17 @@
+function switch_mode() {
+    let mode = document.documentElement;
+
+    // if (mode.classList.contains("light")){
+    //     mode.classList.remove("light");
+    // } else {
+    //     mode.classList.add("light");}
+
+    mode.classList.toggle("light");
+
+    let image = document.getElementById("avatar");
+    if (mode.classList.contains("light")){
+        image.setAttribute("src", "../assets/profile-light.png");
+    } else {
+        image.setAttribute("src", "../assets/profile.png");
+    }
+}
